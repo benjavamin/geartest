6addb11d52e0b43d79d0893c0d5d2b54afcbba09
Benjamin Bajic <bajic.benjamin98@gmail.com>
2019-05-12 14:10:48 +0200

    core backend of project

---
+++ backend/geartest/src/main/java/com/geartest/www/geartest/GeartestApplication.java
@@ -0,0 +1,13 @@
+package com.geartest.www.geartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeartestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeartestApplication.class, args);
+	}
+
+}
